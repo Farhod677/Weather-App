46665ac95c224b8ad47b66b6327e481d7cc07372
Farhod Ubaydullaev <ubaydullaevfarhod18@gmail.com>
2021-08-31 11:16:43 +0500

    second commit

--- src/store/actions/cityAction.js
+++ src/store/actions/cityAction.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const loadCity = (location) => async (dispatch) => {
-    const current = await axios.get(`http://api.weatherapi.com/v1/current.json?key=43bcd4ae68144414b42102242213008&q=${location}`);
+    const current = await axios.get(`https://api.weatherapi.com/v1/current.json?key=43bcd4ae68144414b42102242213008&q=${location}`);
     dispatch({
         type: 'FETCH_CITY',
         payload: {
